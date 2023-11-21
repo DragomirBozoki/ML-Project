431cd347195318812f7990d328952e0b3851ccc6
Dragomir01 <bozokidragomir@gmail.com>
2023-11-21 19:15:52 +0100

    Updates

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ df = pd.read_csv('lungcancer.csv')
 #shape baze
 print(df.shape)
 print('\n\n')
+
 #Glavni parametri baze
 print(df.describe().T)
 
@@ -26,13 +27,16 @@ df.drop(['index', 'Patient Id'], inplace=True, axis = 1)
 fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 
 axs[0].pie(df['Level'].value_counts(), labels=df['Level'].value_counts().index)
-axs[0].set_title('Distribucija nivoa ')
+axs[0].set_title('Distribucija nivoa')
 
 axs[1].pie(df['Gender'].value_counts(), labels=df['Gender'].value_counts().index)
 axs[1].set_title('Distribucij a polova')
 
 plt.tight_layout()
 plt.show()
+
+
+
 
 #Transformisanje objekt vrednosti u redne za izlaznu y - level
 level_mapping = {'High' : 3,
@@ -46,6 +50,19 @@ print("")
 corr = df.corr()
 f = plt.figure(figsize=(15, 9))
 sns.heatmap(corr, annot=True)
+plt.show()
+
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+
+df.plot.scatter(x='Coughing of Blood', y='Level', ax=axes[0, 0], title='Coughing of Blood vs Risk Level', c= 'black')
+df.plot.scatter(x='Genetic Risk', y='Level', ax=axes[0, 1], title='Genetic Risk vs Risk Level', c= 'black')
+df.plot.scatter(x='Smoking', y='Level', ax=axes[0, 2], title='Smoking vs Risk Level', c= 'black')
+#==============================
+df.plot.scatter(x='Fatigue', y='Level', ax=axes[1, 0], title='Fatigue vs Risk Level', c= 'black')
+df.plot.scatter(x='Dust Allergy', y='Level', ax=axes[1, 1], title='Dust Allergy vs Risk Level', c= 'black')
+df.plot.scatter(x='Chest Pain', y='Level', ax=axes[1, 2], title='Chest Pain vs Risk Level', c= 'black')
+
+plt.tight_layout()
 plt.show()
 
 #Izbacivanje izlazne iz skupa X za obuku i trening
