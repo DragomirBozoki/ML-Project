55d5cd5d1d5bbdfd2cabae754d33b16bf98dca93
DragomirBozoki <82651750+DragomirBozoki@users.noreply.github.com>
2023-11-24 17:24:46 +0100

    dodao

--- main.py
+++ main.py
@@ -23,20 +23,21 @@ print('Nulls: ', nulls)
 #Odbacivanje kolona sa nekorisnim podacima
 df.drop(['index', 'Patient Id'], inplace=True, axis = 1)
 
+levelage = sns.FacetGrid(df, col = 'Level', height=4)
+levelage.map(sns.distplot, "Age")
+plt.show()
+
+sea = sns.FacetGrid(df, col = 'Level', height=4)
+sea.map(sns.distplot, "Gender")
+plt.show()
+
 #Plotovanje izlazne Level da bi se videlo koliko vrednosti pripada kojoj grupi
-fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 
-axs[0].pie(df['Level'].value_counts(), labels=df['Level'].value_counts().index)
-axs[0].set_title('Distribucija nivoa')
-
-axs[1].pie(df['Gender'].value_counts(), labels=df['Gender'].value_counts().index)
-axs[1].set_title('Distribucij a polova')
+plt.pie(df['Level'].value_counts(), labels=df['Level'].value_counts().index)
+plt.set_title('Distribucija nivoa')
 
 plt.tight_layout()
 plt.show()
-
-
-
 
 #Transformisanje objekt vrednosti u redne za izlaznu y - level
 level_mapping = {'High' : 3,
@@ -52,18 +53,6 @@ f = plt.figure(figsize=(15, 9))
 sns.heatmap(corr, annot=True)
 plt.show()
 
-fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
-
-df.plot.scatter(x='Coughing of Blood', y='Level', ax=axes[0, 0], title='Coughing of Blood vs Risk Level', c= 'black')
-df.plot.scatter(x='Genetic Risk', y='Level', ax=axes[0, 1], title='Genetic Risk vs Risk Level', c= 'black')
-df.plot.scatter(x='Smoking', y='Level', ax=axes[0, 2], title='Smoking vs Risk Level', c= 'black')
-#==============================
-df.plot.scatter(x='Fatigue', y='Level', ax=axes[1, 0], title='Fatigue vs Risk Level', c= 'black')
-df.plot.scatter(x='Dust Allergy', y='Level', ax=axes[1, 1], title='Dust Allergy vs Risk Level', c= 'black')
-df.plot.scatter(x='Chest Pain', y='Level', ax=axes[1, 2], title='Chest Pain vs Risk Level', c= 'black')
-
-plt.tight_layout()
-plt.show()
 
 #Izbacivanje izlazne iz skupa X za obuku i trening
 X = df.drop(['Level'], axis = 1)
